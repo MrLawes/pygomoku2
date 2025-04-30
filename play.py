68ad54b3448bdd113d051c43d5a327568df9c785
chenhaiou <chenhaiou@qusong.com>
2025-04-30 16:36:07 +0800

    [play] 调整坐标为 x, y, 便于后面的棋谱统一

--- play.py
+++ play.py
@@ -33,41 +33,27 @@ class Pygomoku:
 
     def check_win(self, row: int, col: int):
         """ 判断是否有五子连珠 """
-
-        # print(f'{row=}; {col=}')
+        print(f"[check_win] {row=};{col=}")
+        print(f"{self.board=}")
 
         data_left, data_right, data_up, data_down = "", "", "", ""
         for i in range(9):
 
             # 收集左边的棋子
             if 0 <= col - i < self.board_size:
-                # print(f'0000 {row=}; {col - i=}')
                 data_left += self.board[row][col - i]
 
             # 收集右边的棋子
             if 0 <= col + i < self.board_size:
-                # print(f'0000 {row=}; {col - i=}')
                 data_right += self.board[row][col + i]
 
             # 收集上边的棋子
             if 0 <= row - i < self.board_size:
-                data_up += self.board[row - 1][col]
+                data_up += self.board[row][col]
 
             # 收集下边的棋子
             if 0 <= row + i < self.board_size:
-                data_down += self.board[row + 1][col]
-
-            # print(f'{data_left=}')
-            # print(f'{data_right=}')
-            # print(f'{data_up=}')
-            # print(f'{data_down=}')
-
-        # print(f'1111  {row=}; {col=}')
-        # print("#" * 40)
-
-        # for b in self.board:
-        #     print(b)
-        #     print("\n")
+                data_down += self.board[row][col]
 
         directions = [(1, 0), (0, 1), (1, 1), (1, -1)]
         for dx, dy in directions:
@@ -106,21 +92,25 @@ class Pygomoku:
                     x_pos = x_pos - 1  # noqa
                     y_pos = y_pos - 3  # noqa
                     print(f'{x_pos=};{y_pos=}')
-                    i = round(x_pos / self.cell_size) - 1
-                    j = round(y_pos / self.cell_size) - 1
-                    if 0 <= i < self.board_size and 0 <= j < self.board_size and self.board[j][i] == u"　  ":
+                    i = x = round(x_pos / self.cell_size) - 1
+                    j = y = 15 - round(y_pos / self.cell_size)
+                    print(f'{x=};{y=}')
+                    if 0 <= i < self.board_size and 0 <= j < self.board_size and self.board[x][y] == u"　  ":
                         print(f"{i=};{j=};{self.current_player=};{self.board[i][j]}")
-                        self.board[j][i] = self.current_player
+                        self.board[x][y] = self.current_player
+
+                        center = ((i + 1) * self.cell_size, (15 - j) * self.cell_size)
+                        print(f"{center=}")
 
                         # 绘制棋子
-                        if "黑" in self.board[j][i]:
-                            pygame.draw.circle(self.screen, self.BLACK, ((i + 1) * self.cell_size, (j + 1) * self.cell_size), self.cell_size // 2 - 2)
-                        elif "白" in self.board[j][i]:
-                            pygame.draw.circle(self.screen, self.WHITE, ((i + 1) * self.cell_size, (j + 1) * self.cell_size), self.cell_size // 2 - 2)
+                        if "黑" in self.board[x][y]:
+                            pygame.draw.circle(self.screen, self.BLACK, center, self.cell_size // 2 - 2)
+                        elif "白" in self.board[x][y]:
+                            pygame.draw.circle(self.screen, self.WHITE, center, self.cell_size // 2 - 2)
 
                         # print(1, f'{i=};{j=}; ')  # todo delete
-                        if self.check_win(j, i):
-                            # print(f"玩家 {self.current_player} 获胜！")
+                        if self.check_win(x, y):
+                            print(f"玩家 {self.current_player} 获胜！")
                             running = False
                         # 换对手下
                         self.current_player = "白" if self.current_player == "黑" else "黑"
